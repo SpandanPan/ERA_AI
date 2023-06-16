a973ce86f875191fcfcba42afe642738e788e792
SpandanPan <83960135+SpandanPan@users.noreply.github.com>
2023-06-17 02:49:29 +0530

    Update model_v1.py

--- model_v1.py
+++ model_v1.py
@@ -1,1 +1,177 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class Model1(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+
+            nn.ReLU()
+        ) # output_size = 26
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
+
+            nn.ReLU()
+        ) # output_size = 24
+
+        # TRANSITION BLOCK 1
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 12
+
+        # CONVOLUTION BLOCK 2
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
+
+
+            nn.ReLU()
+        ) 
+
+        # OUTPUT BLOCK
+       
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU()
+        ) 
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(10, 10), padding=0, bias=False),
+
+        ) 
+
+
+
+dropout_value = 0.1
+
+class Model2(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 26
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
+
+            nn.ReLU(),
+            nn.BatchNorm2d(20),
+            nn.Dropout(dropout_value)
+        ) 
+
+        # TRANSITION BLOCK 1
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            #nn.ReLU()
+
+        ) 
+
+        # CONVOLUTION BLOCK 2
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=15, kernel_size=(3, 3), padding=0, bias=False),
+
+
+            nn.ReLU(),
+            nn.BatchNorm2d(15),
+            nn.Dropout(dropout_value)
+        ) 
+
+        # OUTPUT BLOCK
+       # self.pool2 = nn.MaxPool2d(2, 2)
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=15, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(20),
+            nn.Dropout(dropout_value)
+        ) 
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=8)
+        ) 
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+
+        ) 
+
+
+dropout_value = 0.1
+
+class Model3(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 26
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 24
+
+        # TRANSITION BLOCK 1
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            #nn.ReLU()
+
+        ) # output_size = 12
+
+        # CONVOLUTION BLOCK 2
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=14, kernel_size=(3, 3), padding=0, bias=False),
+
+
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout(dropout_value)
+        ) # output_size = 10
+
+        # OUTPUT BLOCK
+       # self.pool2 = nn.MaxPool2d(2, 2)
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=14, out_channels=18, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(18),
+            nn.Dropout(dropout_value)
+        ) # output_size = 10
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=18, out_channels=12, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(12),
+            nn.Dropout(dropout_value)
+        ) # output_size = 10
+
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=6)
+        ) # output_size = 1
+
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=12, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+
+        ) # output_size = 1
+
+
 
